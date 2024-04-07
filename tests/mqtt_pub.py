f51ed2c9a8c0bd94253f20d0ab6efb17e9dd8ff2
coldinke <yangzhibo.yang@gmail.com>
2024-04-07 20:59:23 +0800

    feat(tests): update the test case

--- tests/mqtt_pub.py
+++ tests/mqtt_pub.py
@@ -1,10 +1,19 @@
 import time
+import json
 import paho.mqtt.client as client
+import paho.mqtt.publish as publish
+
+
+
+# MQTT回调函数
+# def on_connect(client, userdata, flags, rc):
+#     print("Connected with result code "+str(rc))
 
 def on_publish(client, userdata, mid, reason_code, properties):
     # reason_code and properties will only be present in MQTTv5. It's always unset in MQTTv3
     try:
-        userdata.remove(mid)
+        # userdata.pop()
+        print("sent done...")
     except KeyError:
         print("on_publish() is called with a mid not present in unacked_publish")
         print("This is due to an unavoidable race-condition:")
@@ -18,29 +27,43 @@ def on_publish(client, userdata, mid, reason_code, properties):
         print("We could also try using a list of acknowledged mid rather than removing from pending list,")
         print("but remember that mid could be re-used !")
 
-unacked_publish = set()
-mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+# unacked_publish = set()
+
+sensor_data = [
+    {"nodeno": "1", "temp": 10.1, "humi": 10.1},
+    {"nodeno": "2", "temp": 11.1, "humi": 12.1},
+    {"nodeno": "3", "temp": 12.1, "humi": 13.1},
+    {"nodeno": "4", "temp": 13.1, "humi": 14.1},
+    {"nodeno": "5", "temp": 14.1, "humi": 14.1},
+    {"nodeno": "6", "temp": 15.1, "humi": 15.1},
+    {"nodeno": "7", "temp": 16.1, "humi": 16.1},
+    {"nodeno": "8", "temp": 17.1, "humi": 17.1},
+]
+
+json_data = dict()
+
+for i, data in enumerate(sensor_data, start=1):
+    json_data[f"no{i}"] = data
+
+json_str = json.dumps(json_data)
+print(json_str)
+
+mqttc = client.Client(client.CallbackAPIVersion.VERSION2)
+# mqttc.on_connect = on_connect
 mqttc.on_publish = on_publish
 
-mqttc.user_data_set(unacked_publish)
-mqttc.connect("mqtt.eclipseprojects.io")
+mqttc.user_data_set(json_str)
+mqttc.username_pw_set("user01", "1234")
+mqttc.connect("192.168.25.80", 1883, 60)
 mqttc.loop_start()
 
 # Our application produce some messages
-msg_info = mqttc.publish("paho/test/topic", "my message", qos=1)
-unacked_publish.add(msg_info.mid)
-
-msg_info2 = mqttc.publish("paho/test/topic", "my message2", qos=1)
-unacked_publish.add(msg_info2.mid)
-
-# Wait for all message to be published
-while len(unacked_publish):
-    time.sleep(0.1)
+msg_info = mqttc.publish("test/mqtt", json_str, qos=1)
 
 # Due to race-condition described above, the following way to wait for all publish is safer
 msg_info.wait_for_publish()
-msg_info2.wait_for_publish()
 
 mqttc.disconnect()
 mqttc.loop_stop()
 
+# publish.single("test/mqtt", json_str, hostname="192.168.25.80")
--- tests/mqtt_sub.py
+++ tests/mqtt_sub.py
@@ -1,44 +1,26 @@
 import paho.mqtt.client as mqtt
 
-def on_subscribe(client, userdata, mid, reason_code_list, properties):
-    # Since we subscribed only for a single channel, reason_code_list contains
-    # a single entry
-    if reason_code_list[0].is_failure:
-        print(f"Broker rejected you subscription: {reason_code_list[0]}")
-    else:
-        print(f"Broker granted the following QoS: {reason_code_list[0].value}")
-
-def on_unsubscribe(client, userdata, mid, reason_code_list, properties):
-    # Be careful, the reason_code_list is only present in MQTTv5.
-    # In MQTTv3 it will always be empty
-    if len(reason_code_list) == 0 or not reason_code_list[0].is_failure:
-        print("unsubscribe succeeded (if SUBACK is received in MQTTv3 it success)")
-    else:
-        print(f"Broker replied with failure: {reason_code_list[0]}")
-    client.disconnect()
-
-def on_message(client, userdata, message):
-    # userdata is the structure we choose to provide, here it's a list()
-    userdata.append(message.payload)
-    # We only want to process 10 messages
-    if len(userdata) >= 10:
-        client.unsubscribe("$SYS/#")
-
+# The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, reason_code, properties):
-    if reason_code.is_failure:
-        print(f"Failed to connect: {reason_code}. loop_forever() will retry connection")
-    else:
-        # we should always subscribe from on_connect callback to be sure
-        # our subscribed is persisted across reconnections.
-        client.subscribe("$SYS/#")
+    print(f"Connected with result code {reason_code}")
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("test/mqtt")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqttc.on_connect = on_connect
 mqttc.on_message = on_message
-mqttc.on_subscribe = on_subscribe
-mqttc.on_unsubscribe = on_unsubscribe
 
-mqttc.user_data_set([])
-mqttc.connect("mqtt.eclipseprojects.io")
+mqttc.username_pw_set("user01", "1234")
+#mqttc.connect("127.0.0.1", 1883, 60)
+mqttc.connect("ubuntu1.orb.local", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
 mqttc.loop_forever()
-print(f"Received the following message: {mqttc.user_data_get()}")
